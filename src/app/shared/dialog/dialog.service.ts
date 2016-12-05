21de14fb8458b1fb920d9e9436aff6dba5aa9b0d
dancancro <dan.cancro@gmail.com>
2016-12-05 13:53:28 -0500

    figuring out styles. stuck on tsconfig.json issue

---
+++ src/app/shared/dialog/dialog.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+/**
+ * Async modal dialog service
+ * DialogService makes this app easier to test by faking this service.
+ * TODO: better modal implementation that doesn't use window.confirm
+ */
+@Injectable()
+export class DialogService {
+  /**
+   * Ask user to confirm an action. `message` explains the action and choices.
+   * Returns promise resolving to `true`=confirm or `false`=cancel
+   */
+  confirm(message?: string) {
+    return new Promise<boolean>(resolve => {
+      return resolve(window.confirm(message || 'Is it OK?'));
+    });
+  };
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
